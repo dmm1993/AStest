d56fd9b55a41f641e93f22505d62369cb1b8b54e
Administrator <1076697512@qq.com>
2015-12-10 11:45:01 +0800

    大大的事

---
+++ app/src/main/java/com/sz1993/dmm/astest/MainActivity.java
@@ -0,0 +1,14 @@
+package com.sz1993.dmm.astest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //第一次修改注释
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
